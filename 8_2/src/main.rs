5380301c58c34576b39565741653a30f301f92a2
Maxwell Burson <maxwellburson@gmail.com>
2022-12-20 19:13:20 -0800

    Slower but right

--- 8_2/src/main.rs
+++ 8_2/src/main.rs
@@ -12,6 +12,52 @@ enum Direction {
     Right,
 }
 
+fn calc_score(forest: &Vec<Vec<Tree>>, x: usize, y: usize) -> usize {
+    let top = (0..y)
+        .rev()
+        .find_map(|y2| {
+            if forest[y2][x].height >= forest[y][x].height {
+                Some(y - y2)
+            } else {
+                None
+            }
+        })
+        .unwrap_or(y);
+    let left = (0..x)
+        .rev()
+        .find_map(|x2| {
+            // dbg!(x, x2);
+            if forest[y][x2].height >= forest[y][x].height {
+                Some(x - x2)
+            } else {
+                None
+            }
+        })
+        .unwrap_or(x);
+    let bottom = ((y + 1)..forest.len())
+        .find_map(|y2| {
+            if forest[y2][x].height >= forest[y][x].height {
+                Some(y2 - y)
+            } else {
+                None
+            }
+        })
+        .unwrap_or(forest.len() - 1 - y);
+    let right = ((x + 1)..forest[0].len())
+        .find_map(|x2| {
+            if forest[y][x2].height >= forest[y][x].height {
+                Some(x2 - x)
+            } else {
+                None
+            }
+        })
+        .unwrap_or(forest[0].len() - 1 - x);
+
+    // dbg!(top, left, bottom, right);
+
+    top * left * bottom * right
+}
+
 fn sight_lines(
     forest: &Vec<Vec<Tree>>,
 ) -> Vec<(Direction, Box<dyn Iterator<Item = (usize, usize)>>)> {
@@ -30,7 +76,7 @@ fn sight_lines(
     }
 
     for x in 0..forest[0].len() {
-        println!("x: {x}");
+        // println!("x: {x}");
         sight_lines.push((
             Direction::Down,
             Box::new((0..forest[0].len()).map(move |y| (x, y))),
@@ -51,9 +97,9 @@ struct VisibleTree {
     height: usize,
 }
 
-fn total_visible(input: &str) -> usize {
-    // [y][x]
-    let mut forest: Vec<Vec<Tree>> = input
+// [y][x]
+fn build_forest(input: &str) -> Vec<Vec<Tree>> {
+    input
         .lines()
         .map(|line| {
             line.chars()
@@ -63,7 +109,11 @@ fn total_visible(input: &str) -> usize {
                 })
                 .collect()
         })
-        .collect();
+        .collect()
+}
+
+fn total_visible_fast(input: &str) -> usize {
+    let mut forest = build_forest(input);
 
     let width = forest[0].len();
     let height = forest.len();
@@ -113,7 +163,10 @@ fn total_visible(input: &str) -> usize {
                 y,
             });
             tree.score *= view_distance;
-            println!("view_distance: {view_distance}, score: {:#?}, visible: {:#?}", tree.score, &visible_trees);
+            println!(
+                "view_distance: {view_distance}, score: {:#?}, visible: {:#?}",
+                tree.score, &visible_trees
+            );
             println!();
         }
     }
@@ -133,12 +186,43 @@ fn total_visible(input: &str) -> usize {
     best.2.score
 }
 
+fn total_visible_slow(input: &str) -> usize {
+    let forest = build_forest(input);
+
+    let width = forest[0].len();
+    let height = forest.len();
+
+    let mut max_score = 0;
+
+    for x in 0..width {
+        for y in 0..height {
+            let score = calc_score(&forest, x, y);
+            if score > max_score {
+                max_score = score;
+            }
+        }
+    }
+
+    max_score
+}
+
 #[test]
 fn example() {
-    assert_eq!(total_visible(include_str!("../example.txt")), 8);
+    assert_eq!(total_visible_fast(include_str!("../example.txt")), 8);
+    assert_eq!(total_visible_slow(include_str!("../example.txt")), 8);
+}
+
+#[test]
+fn basic_debug() {
+    let example_forest = build_forest(include_str!("../example.txt"));
+    assert_eq!(calc_score(&example_forest, 2, 3), 8);
+
+    let input_forest = build_forest(include_str!("../input.txt"));
+    let res = calc_score(&input_forest, 3, 2);
+    println!("res {res}")
 }
 
 fn main() {
-    let result = total_visible(include_str!("../input.txt"));
+    let result = total_visible_slow(include_str!("../input.txt"));
     println!("result: {result}");
 }
