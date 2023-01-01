9b08000b47708d21344a27cbcd02ea33f1ef9400
Maxwell Burson <maxwellburson@gmail.com>
2022-12-31 23:28:04 -0800

    Finished day 13 2

---
+++ 13_2/src/main.rs
@@ -0,0 +1,192 @@
+use std::cmp::Ordering;
+
+use either::Either::{self, Left, Right};
+
+#[derive(Clone, Debug)]
+struct List(Vec<Either<usize, Box<List>>>);
+type Packet = List;
+
+impl List {
+   fn print(&self) {
+        print!("[");
+        for (i, c) in self.0.iter().enumerate() {
+            if i != 0 {
+                print!(",");
+            }
+            match c {
+                Left(c) => print!("{}", c),
+                Right(c) => c.print(),
+            }
+        }
+        print!("]");
+    }
+}
+
+#[derive(Debug)]
+struct Input(Vec<Packet>);
+
+impl Input {
+    fn print(&self) {
+        for packet in self.0.iter() {
+            packet.print();
+            println!();
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum Sorted {
+    True,
+    TrueFinal,
+    False,
+}
+
+fn parse_digit(input: &mut std::iter::Peekable<std::str::Chars>) -> usize {
+    let mut chars: Vec<char> = vec![];
+
+    while let Some(v) = input.next_if(|c| c.is_ascii_digit()) {
+        chars.push(v);
+    }
+
+    chars.iter().collect::<String>().parse().unwrap()
+}
+
+fn parse_list(input: &mut std::iter::Peekable<std::str::Chars>) -> List {
+    input.next();
+    let mut list = List(vec![]);
+    'outer: loop {
+        let next = input.peek().unwrap();
+        dbg!(next);
+        match next {
+            ']' => {
+                input.next();
+                break 'outer;
+            }
+            ',' => {
+                input.next();
+            }
+            '[' => {
+                list.0.push(either::Right(Box::new(parse_list(input))));
+            }
+            c => {
+                if c.is_ascii_digit() {
+                    list.0.push(either::Left(parse_digit(input)));
+                } else {
+                    panic!("Unexpected {c}");
+                }
+            }
+        };
+    }
+
+    list
+}
+
+fn parse_input(input: &str) -> Input {
+    let packets = input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| parse_list(&mut l.chars().peekable()));
+
+    Input(packets.collect())
+}
+
+fn are_list_items_ordered(a: &Either<usize, Box<List>>, b: &Either<usize, Box<List>>) -> Sorted {
+    if let (Left(a), Left(b)) = (a, b) {
+        return if a == b {
+            Sorted::True
+        } else if a > b {
+            Sorted::False
+        } else {
+            Sorted::TrueFinal
+        };
+    }
+
+    let a: List = match a {
+        Left(_) => List(vec![a.clone()]),
+        Right(a) => a.as_ref().clone(),
+    };
+
+    let b: List = match b {
+        Left(_) => List(vec![b.clone()]),
+        Right(b) => b.as_ref().clone(),
+    };
+
+    are_lists_ordered(&a, &b)
+}
+
+fn are_lists_ordered(a: &List, b: &List) -> Sorted {
+    for (i, left) in a.0.iter().enumerate() {
+        let right = b.0.get(i);
+        match right {
+            Some(right) => {
+                let sorted = are_list_items_ordered(&left, &right);
+                if sorted == Sorted::False || sorted == Sorted::TrueFinal {
+                    return sorted;
+                }
+            }
+            None => return Sorted::False,
+        }
+    }
+
+    if a.0.len() == b.0.len() {
+        Sorted::True
+    } else {
+        Sorted::TrueFinal
+    }
+}
+
+fn solve(input: &str) -> usize {
+    let mut input = parse_input(&input);
+
+    let divider_1: Packet = List(vec![Right(Box::new(List(vec![Left(2)])))]);
+    let divider_2: Packet = List(vec![Right(Box::new(List(vec![Left(6)])))]);
+
+    let div_1_ptr = divider_1.0.as_ptr() as usize;
+    let div_2_ptr = divider_2.0.as_ptr() as usize;
+
+    input.0.push(divider_1);
+    input.0.push(divider_2);
+
+    input.0.sort_by(|left, right| {
+        if are_lists_ordered(&left, &right) == Sorted::False {
+            Ordering::Greater
+        } else {
+            Ordering::Less
+        }
+    });
+
+    let index1 = input
+        .0
+        .iter()
+        .enumerate()
+        .find(|(_, item)| item.0.as_ptr() as usize == div_1_ptr)
+        .unwrap()
+        .0
+        + 1;
+    let index2 = input
+        .0
+        .iter()
+        .enumerate()
+        .find(|(_, item)| item.0.as_ptr() as usize == div_2_ptr)
+        .unwrap()
+        .0
+        + 1;
+
+    println!();
+    input.print();
+    println!();
+
+    dbg!(index1, index2);
+
+    index1 * index2
+}
+
+#[test]
+fn example() {
+    assert_eq!(solve(include_str!("../example.txt")), 140);
+}
+
+fn main() {
+    let result = solve(include_str!("../input.txt"));
+    println!("result: {result}");
+}
